6be966c6e4c91714a6c8c8a92a6069465537822e
jac11 <rootx1982@gmail.com>
2022-01-21 02:26:37 +0400

    Add files via upload

--- reachable.py
+++ reachable.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 import argparse
 import sys
 from Package.Banner import *
 import os
+import subprocess
 
 S='\033[0m' 
 W = "\033[1;37m"
@@ -15,49 +17,66 @@ Y='\033[1;33m'
 
 class Run :
         print(Banner)
+        def Check_InterFace(self):
+            if self.args.Interface:
+              try:
+                  commad_ifconfig = "ifconfig | grep  "+ self.args.Interface
+                  commad_ifconfig = subprocess.check_output (commad_ifconfig,shell=True).decode('utf-8')
+              except Exception:
+                     print(B+"\n[+] Device [ "+Y +self.args.Interface+B+ " ] does not exist \n")
+                     print(W+D+I+"\n[*] Device available-\n"+R+"="*14+"\n")
+                     all_Interface = os.listdir('/sys/class/net/') 
+                     for i in all_Interface :
+                          print(B+'[+] Interface : '+Y, i)
+                     print()    
+                     exit()                         
         def __init__(self):
-            self.args_command()  
+            self.args_command() 
+            self.Check_InterFace()
             self.control()
         def control(self):
-           if self.args.network and not self.args.start and not self.args.end and not self.args.Interface:
+           if self.args.Pnetwork and not self.args.start and not self.args.end :
               from Package.Pingclass import  Discover_Network
               print("--PING-SCAN"+"\n"+"+"*20)
               run = Discover_Network()
               exit()
-           elif self.args.start and self.args.end and not self.args.Interface and not self.args.Interface:
+           elif self.args.Pnetwork and  self.args.start and self.args.end :
               from Package.HostRange import RangeOfHosts
               print("--PING-SCAN"+"\n"+"+"*20)
               run = RangeOfHosts()
               exit()
-           elif self.args.Host and not self.args.Interface:
+           elif self.args.phost :
                  from Package.OneHost import  Host_One
                  print("--PING-SCAN"+"\n"+"+"*20)
                  run = Host_One()
                  exit()
-           elif self.args.network and not self.args.start and not self.args.end and self.args.Interface:
+           elif self.args.arpnetwork and not self.args.start and not self.args.end :
               from ARP_PACK.arp_class import Arp_Network
               print("--ARP-SCAN"+"\n"+"+"*20)
               run = Arp_Network()
               exit()
-           elif self.args.start and self.args.end and self.args.Interface :
+           elif self.args.arpnetwork and self.args.start and self.args.end and not self.args.Pnetwork :
               from ARP_PACK.arp_range import Range_arp_host
               print("--ARP-SCAN"+"\n"+"+"*20)
               run = Range_arp_host()
               exit()
-           elif self.args.Host and self.args.Interface:             
+           elif self.args.ahost :             
                  from ARP_PACK.arp_one_Host import  Arp_Host_One
                  print(B+"--ARP-SCAN"+"\n"+"+"*20)
                  run = Arp_Host_One()
                  exit()      
         def args_command(self):
-              print(B+"")
+              print(Y+"")
               parser = argparse.ArgumentParser( description="Usage: <OPtion> <arguments> ")
-              parser.add_argument( '-N',"--network"   ,metavar='' , action=None ,help ="ping all Network ,IPaddress/prefix ")
-              parser.add_argument( '-O',"--output"    ,metavar='' , action=None  ,help ="output file report ")
-              parser.add_argument( '-S',"--start"     ,metavar='' , action=None  ,help ="start of the range Ips ")
-              parser.add_argument( '-H',"--Host"      ,metavar='' , action=None  ,help ="Ping One Host Only ")
-              parser.add_argument( '-E',"--end"       ,metavar='' , action=None  ,help ="end of the range ips ")
-              parser.add_argument( '-I',"--Interface"       ,metavar='' , action=None  ,help ="Interface use for arp scan ")
+              parser.add_argument( '-PN',"--Pnetwork"     ,metavar='' , action=None ,help ="ping all Network ,IPaddress/prefix ")
+              parser.add_argument( '-AN',"--arpnetwork"   ,metavar='' , action=None ,help ="arp scan  all Network ,IPaddress/prefix ")
+              parser.add_argument( '-O',"--output"        ,metavar='' , action=None  ,help ="output file report ")
+              parser.add_argument( '-S',"--start"         ,metavar='' , action=None  ,help ="start of the range Ips ")
+              parser.add_argument( '-PH',"--phost"          ,metavar='' , action=None  ,help ="Ping One Host Only ")
+              parser.add_argument( '-AH',"--ahost"          ,metavar='' , action=None  ,help ="arp  One Host Only ")
+              parser.add_argument( '-E',"--end"           ,metavar='' , action=None  ,help ="end of the range ips ")
+              parser.add_argument( '-M',"--Mac"           ,metavar='' , action=None  ,help ="end of the range ips ")
+              parser.add_argument( '-I',"--Interface"     ,metavar='' , action=None  ,help =" User Interface  ")
               self.args = parser.parse_args()
               if len(sys.argv)> 1 :
                    
