e80c6c681e0a9dde77e8126e8a6b9f4f30fab234
jac11 <rootx1982@gmail.com>
2022-01-23 17:59:35 +0400

    Add files via upload

--- reachable.py
+++ reachable.py
@@ -36,7 +36,7 @@ class Run :
             self.control()
         def control(self):
            if self.args.Pnetwork and not self.args.start and not self.args.end :
-              from Package.Pingclass import  Discover_Network
+             
               print("--PING-SCAN"+"\n"+"+"*20)
               run = Discover_Network()
               exit()
@@ -64,7 +64,9 @@ class Run :
                  from ARP_PACK.arp_one_Host import  Arp_Host_One
                  print(B+"--ARP-SCAN"+"\n"+"+"*20)
                  run = Arp_Host_One()
-                 exit()      
+                 exit()  
+           
+                       
         def args_command(self):
               print(Y+"")
               parser = argparse.ArgumentParser( description="Usage: <OPtion> <arguments> ")
@@ -72,42 +74,25 @@ class Run :
               parser.add_argument( '-AN',"--arpnetwork"   ,metavar='' , action=None ,help ="arp scan  all Network ,IPaddress/prefix ")
               parser.add_argument( '-O',"--output"        ,metavar='' , action=None  ,help ="output file report ")
               parser.add_argument( '-S',"--start"         ,metavar='' , action=None  ,help ="start of the range Ips ")
-              parser.add_argument( '-PH',"--phost"          ,metavar='' , action=None  ,help ="Ping One Host Only ")
-              parser.add_argument( '-AH',"--ahost"          ,metavar='' , action=None  ,help ="arp  One Host Only ")
+              parser.add_argument( '-PH',"--phost"        ,metavar='' , action=None  ,help ="Ping One Host Only ")
+              parser.add_argument( '-AH',"--ahost"        ,metavar='' , action=None  ,help ="arp  One Host Only ")
               parser.add_argument( '-E',"--end"           ,metavar='' , action=None  ,help ="end of the range ips ")
-              parser.add_argument( '-M',"--Mac"           ,metavar='' , action=None  ,help ="end of the range ips ")
+              parser.add_argument( '-M',"--Mac"           ,metavar='' , action=None  ,help ="Change host mac addreess befor Scan  ")
               parser.add_argument( '-I',"--Interface"     ,metavar='' , action=None  ,help =" User Interface  ")
+              parser.add_argument( '-i',"--info"          ,metavar='' , action=None  ,help =" print More info   ")
               self.args = parser.parse_args()
               if len(sys.argv)> 1 :
-                   
                    pass
-              else:
-                   parser.print_help()
-                   print(R+"="*15+S)
-                   print(R+"-Example ping scan :-"+"\n"+"="*10+S)
-                   print(Y+"-To Scan all Subnet Use -N <network/prefix>\n./reachable.py -N 10.195.100.200/25")               
-                   print(R+"\t\t"+"="*20+'\n')
-                   print(Y+"-To Scan range of ips Use -N <network/prefix> -S <Start>  -E <end>\n./reachable.py -N 10.195.100.200/24 -S 240 -E 254 ")               
-                   print(R+"\t\t"+"="*20)
-                   print(Y+"-To Scan one Host  Use  '-H' <host ip>\n./reachable.py -H 10.195.100.200/25\nor\n./reachable.py -H 10.196.100.3")
-                   print(R+"\t\t"+"="*20)
-                   print(Y+"-To Save the output into file Use -O <file name>")
-                   print(Y+"./reachable.py -N 10.195.100.200/24 -S 240 -E 254 -O report.txt") 
-                   print(R+"="*15)
-                   print()
-                   print(W+"[*] For arp Scan Use root Login or  sudo privileges ")
-                   print(Y+" -I or --Interface  use ifconfig to make sure that any of the interface are available ") 
-                   print(R+"-Example arp scan-:-\n"+"="*10)
-                   print(Y+"-To Scan all Subnet Use -N <network/prefix> -I < Interface > \nsudo./reachable.py -N 10.195.100.200/25 -I eth0 ")               
-                   print(R+"\t\t"+"="*20)
-                   print(Y+"-To Scan range of ips Use -N <network/prefix> -S <Start>  -E <end>\nsudo./reachable.py -N 10.195.100.200/24  -I eth0 -S 240 -E 254 ")               
-                   print(R+"\t\t"+"="*20)
-                   print(Y+"-To Scan one Host  Use  '-H' <host ip>\n sudo ./reachable.py -H 10.195.100.200/25 -I eth0 \nor\n sudo./reachable.py -H 10.196.100.3 -I wlan0")
-                   print(R+"\t\t"+"="*20)
-                   print(Y+"-To Save the output into file Use -O <file name>")
-                   print(Y+"sudo ./reachable.py -N 10.195.100.200/24  -I eth0 -S 240 -E 254 -O report.txt") 
-                   print(Banner)
+              else:    
+                   parser.print_help()                 
                    exit()
-
+              if self.args.info:
+                 if 'info' in sys.argv and len(sys.argv)==3:
+                    from Package.info import Info_print
+                    run = Info_print()                   
+                 else:
+                      parser.print_help() 
+                      exit()          
+ 
 if __name__=="__main__":
    Run()
